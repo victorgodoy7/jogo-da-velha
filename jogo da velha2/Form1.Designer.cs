c6ae103e2fc618f1fbbfa8d0d9a2d2d467873c55
victor.lgsilva2 <victor.lgsilva2@ITA0557230W10-1>
2024-08-06 19:57:18 -0300

    Adicionar arquivos de projeto.

---
+++ jogo da velha2/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace jogo_da_velha2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            pictureBox1 = new PictureBox();
+            button12 = new Button();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(224, 224, 224);
+            button1.Location = new Point(547, 90);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 112);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(224, 224, 224);
+            button2.Location = new Point(682, 90);
+            button2.Name = "button2";
+            button2.Size = new Size(108, 112);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(224, 224, 224);
+            button3.Location = new Point(812, 90);
+            button3.Name = "button3";
+            button3.Size = new Size(107, 112);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(224, 224, 224);
+            button4.Location = new Point(547, 217);
+            button4.Name = "button4";
+            button4.Size = new Size(107, 112);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(224, 224, 224);
+            button5.Location = new Point(682, 217);
+            button5.Name = "button5";
+            button5.Size = new Size(108, 112);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(224, 224, 224);
+            button6.Location = new Point(812, 217);
+            button6.Name = "button6";
+            button6.Size = new Size(107, 112);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(224, 224, 224);
+            button7.Location = new Point(547, 355);
+            button7.Name = "button7";
+            button7.Size = new Size(107, 112);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(224, 224, 224);
+            button8.Location = new Point(682, 355);
+            button8.Name = "button8";
+            button8.Size = new Size(108, 112);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(224, 224, 224);
+            button9.Location = new Point(812, 355);
+            button9.Name = "button9";
+            button9.Size = new Size(107, 112);
+            button9.TabIndex = 8;
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(255, 255, 192);
+            button10.ForeColor = Color.Black;
+            button10.Location = new Point(630, 488);
+            button10.Name = "button10";
+            button10.Size = new Size(75, 23);
+            button10.TabIndex = 9;
+            button10.Text = "V";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.FromArgb(255, 255, 192);
+            button11.ForeColor = Color.Black;
+            button11.Location = new Point(775, 488);
+            button11.Name = "button11";
+            button11.Size = new Size(75, 23);
+            button11.TabIndex = 10;
+            button11.Text = "O";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(192, 255, 255);
+            pictureBox1.Location = new Point(495, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(467, 510);
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(255, 255, 192);
+            button12.Font = new Font("Showcard Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button12.ForeColor = SystemColors.InactiveCaptionText;
+            button12.Location = new Point(648, 27);
+            button12.Name = "button12";
+            button12.Size = new Size(172, 40);
+            button12.TabIndex = 12;
+            button12.Text = "JOGO DA VELHA";
+            button12.TextAlign = ContentAlignment.TopCenter;
+            button12.TextImageRelation = TextImageRelation.TextAboveImage;
+            button12.UseVisualStyleBackColor = false;
+            button12.UseWaitCursor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(192, 255, 255);
+            pictureBox2.Location = new Point(12, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(467, 510);
+            pictureBox2.TabIndex = 13;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(983, 534);
+            Controls.Add(pictureBox2);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private PictureBox pictureBox1;
+        private Button button12;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ jogo da velha2/Form1.cs
@@ -0,0 +1,35 @@
+namespace jogo_da_velha2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
